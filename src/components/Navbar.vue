1adbafec05746e1e2421cd7c92fa34f1a24f907f
alexander <al@alvox.net>
2019-07-29 11:41:10 +0300

    Add navbar

---
+++ src/components/Navbar.vue
@@ -0,0 +1,35 @@
+<template>
+    <nav class="flex items-center justify-between flex-wrap bg-teal-500 p-4">
+            <div class="flex items-center flex-shrink-0 text-white mr-6">
+                <span class="font-semibold text-xl tracking-tight">FLCRD</span>
+            </div>
+            <div class="w-full block flex-grow lg:flex lg:items-center lg:w-auto">
+                <div class="text-sm lg:flex-grow">
+                    <a href="#responsive-header" class="block mt-4 lg:inline-block lg:mt-0 text-teal-200 hover:text-white mr-4">
+                        Popular
+                    </a>
+                    <a href="#responsive-header" class="block mt-4 lg:inline-block lg:mt-0 text-teal-200 hover:text-white mr-4">
+                        Link 2
+                    </a>
+                    <a href="#responsive-header" class="block mt-4 lg:inline-block lg:mt-0 text-teal-200 hover:text-white">
+                        Link 3
+                    </a>
+                </div>
+                <div>
+                    <a href="#" class="inline-block text-sm px-4 py-2 leading-none border rounded text-white border-white hover:border-transparent hover:text-teal-500 hover:bg-white mt-4 lg:mt-0 mr-4">Log in</a>
+                    <a href="#" class="inline-block text-sm px-4 py-2 leading-none border rounded text-white border-white hover:border-transparent hover:text-teal-500 hover:bg-white mt-4 lg:mt-0">Register</a>
+                </div>
+            </div>
+
+    </nav>
+</template>
+
+<script>
+    export default {
+        name: "Navbar"
+    }
+</script>
+
+<style>
+
+</style>
