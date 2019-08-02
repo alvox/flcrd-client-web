cf088e599c116773e020f68570240aa6fe3b1d63
alexander <al@alvox.net>
2019-08-02 18:37:27 -0400

    Use axios to fetch data from the server.

--- src/components/Index.vue
+++ src/components/Index.vue
@@ -1,7 +1,7 @@
 <template>
     <div class="index container mx-auto">
-        <div class="flex flex-wrap">
-            <div class="w-1/3 p-2" v-for="deck in decks" :key="deck.id">
+        <div class="flex flex-wrap bg-gray-100 items-stretch">
+            <div class="w-1/3 p-2 self-stretch" v-for="deck in decks" :key="deck.id">
                 <div class="flex-1 text-gray-700 bg-gray-200 rounded px-4 py-2 m-2">
                     <p class="text-2xl">{{ deck.name }}</p>
                     <p> {{ deck.description }}</p>
@@ -17,45 +17,19 @@
         name: 'Index',
         data() {
             return {
-                decks: [
-                    {
-                        id: "1",
-                        name: "English words",
-                        description: "Some rarely-used English words"
-                    },
-                    {
-                        id: "2",
-                        name: "BBB",
-                        description: "adfdlfjdkjgkghkjfdjf"
-                    },
-                    {
-                        id: "3",
-                        name: "CCC",
-                        description: "Another deck"
-                    },
-                    {
-                        id: "4",
-                        name: "CCC",
-                        description: "Another deck"
-                    },
-                    {
-                        id: "5",
-                        name: "CCC",
-                        description: "Another deck"
-                    },
-                    {
-                        id: "6",
-                        name: "CCC",
-                        description: "Another deck"
-                    },
-                    {
-                        id: "7",
-                        name: "CCC",
-                        description: "Another deck"
-                    }
-
-                ]
+                decks: []
             }
+        },
+        methods: {
+            fetchDecks() {
+                let URL = "https://flashcards.rocks/v0/decks";
+                this.$http.get(URL).then(result => {
+                    this.decks = result.data
+                })
+            }
+        },
+        created() {
+            this.fetchDecks()
         }
     }
 </script>
