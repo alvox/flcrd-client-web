fd84309ef35689c6634aeffa281a8286f4e9d392
alexander <al@alvox.net>
2019-08-07 18:57:06 -0400

    Implement delete flashcard.

--- src/components/Flashcards.vue
+++ src/components/Flashcards.vue
@@ -9,9 +9,10 @@
         </div>
         <div class="flex flex-wrap items-stretch">
             <div class="w-1/3 p-2 self-stretch" v-for="flashcard in flashcards" :key="flashcard.id">
-                <div class="flex-1 text-gray-700 bg-gray-200 rounded px-4 py-2 m-2 cursor-pointer hover:bg-gray-300">
+                <div class="relative flex-1 text-gray-700 bg-gray-200 rounded px-4 py-2 m-2">
                     <p class="text-2xl">{{ flashcard.front }}</p>
                     <p> {{ flashcard.rear }}</p>
+                    <div class="absolute top-0 right-0 text-red-500 hover:text-red-700 cursor-pointer" @click="deleteCard(flashcard.id)">Delete</div>
                 </div>
             </div>
         </div>
@@ -38,6 +39,12 @@
                 let URL = "https://flashcards.rocks/v0/decks/" + this.$route.params.deck_id;
                 this.$http.delete(URL).then(result => {
                     this.$router.push({name: 'Index'})
+                })
+            },
+            deleteCard(id) {
+                let URL = "https://flashcards.rocks/v0/decks/" + this.$route.params.deck_id + "/flashcards/" + id;
+                this.$http.delete(URL).then(result => {
+                    this.fetchFlashcards()
                 })
             }
         },
