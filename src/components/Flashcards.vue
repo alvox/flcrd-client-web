d96ffd0d296e5ad5220fc0a68fb988ae09e3fa6a
alexander <al@alvox.net>
2019-08-07 18:43:48 -0400

    Implement delete deck.

--- src/components/Flashcards.vue
+++ src/components/Flashcards.vue
@@ -5,6 +5,7 @@
             <a href="" class="border px-4 py-2 rounded-lg">
                 <router-link :to="{name: 'AddFlashcard', params: {deck_id: this.$route.params.deck_id}}">Add new flashcard</router-link>
             </a>
+            <span class="text-xl text-red-400" @click="deleteDeck">Delete deck</span>
         </div>
         <div class="flex flex-wrap items-stretch">
             <div class="w-1/3 p-2 self-stretch" v-for="flashcard in flashcards" :key="flashcard.id">
@@ -31,6 +32,12 @@
                 let URL = "https://flashcards.rocks/v0/decks/" + this.$route.params.deck_id + "/flashcards";
                 this.$http.get(URL).then(result => {
                     this.flashcards = result.data
+                })
+            },
+            deleteDeck() {
+                let URL = "https://flashcards.rocks/v0/decks/" + this.$route.params.deck_id;
+                this.$http.delete(URL).then(result => {
+                    this.$router.push({name: 'Index'})
                 })
             }
         },
