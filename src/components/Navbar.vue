8212b98938275f4453a2b57e3c42d68ddfa8c552
alexander <al@alvox.net>
2019-08-08 16:58:54 -0400

    Wrap app title in the router-link

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,32 +1,35 @@
 <template>
-        <div class="bg-white shadow">
-            <div class="mx-auto max-w-5xl">
-                <div class="flex items-center justify-between py-4">
+    <div class="bg-white shadow">
+        <div class="mx-auto max-w-5xl">
+            <div class="flex items-center justify-between py-4">
+                <router-link :to="{name: 'Index'}">
                     <span class="font-semibold text-2xl text-gray-700 tracking-wide">FLCRD</span>
+                </router-link>
+                <!--                    <div class="hidden sm:flex sm:items-center">-->
+                <!--                        <a href="#" class="text-gray-800 text-sm font-semibold hover:text-purple-600 mr-4">Curated</a>-->
+                <!--                        <a href="#" class="text-gray-800 text-sm font-semibold hover:text-purple-600 mr-4">Popular</a>-->
+                <!--                    </div>-->
 
-<!--                    <div class="hidden sm:flex sm:items-center">-->
-<!--                        <a href="#" class="text-gray-800 text-sm font-semibold hover:text-purple-600 mr-4">Curated</a>-->
-<!--                        <a href="#" class="text-gray-800 text-sm font-semibold hover:text-purple-600 mr-4">Popular</a>-->
-<!--                    </div>-->
-
-                    <div class="hidden sm:flex sm:items-center">
-                        <a href="#" class="text-gray-700 text-sm font-semibold hover:text-purple-600 mr-4">Log in</a>
-                        <a href="#" class="text-gray-700 text-sm font-semibold border px-4 py-2 rounded-lg hover:text-purple-600 hover:border-purple-600">Register</a>
-                    </div>
+                <div class="hidden sm:flex sm:items-center">
+                    <a href="#" class="text-gray-700 text-sm font-semibold hover:text-purple-600 mr-4">Log in</a>
+                    <a href="#"
+                       class="text-gray-700 text-sm font-semibold border px-4 py-2 rounded-lg hover:text-purple-600 hover:border-purple-600">Register</a>
                 </div>
+            </div>
 
-                <div class="block sm:hidden bg-white border-t-2 py-2">
-                    <div class="flex flex-col">
-<!--                        <a href="#" class="text-gray-800 text-sm font-semibold hover:text-purple-600 mb-1">Curated</a>-->
-<!--                        <a href="#" class="text-gray-800 text-sm font-semibold hover:text-purple-600 mb-1">Popular</a>-->
-                        <div class="flex justify-between items-center border-t-2 pt-2">
-                            <a href="#" class="text-gray-800 text-sm font-semibold hover:text-purple-600 mr-4">Log in</a>
-                            <a href="#" class="text-gray-800 text-sm font-semibold border px-4 py-1 rounded-lg hover:text-purple-600 hover:border-purple-600">Register</a>
-                        </div>
+            <div class="block sm:hidden bg-white border-t-2 py-2">
+                <div class="flex flex-col">
+                    <!--                        <a href="#" class="text-gray-800 text-sm font-semibold hover:text-purple-600 mb-1">Curated</a>-->
+                    <!--                        <a href="#" class="text-gray-800 text-sm font-semibold hover:text-purple-600 mb-1">Popular</a>-->
+                    <div class="flex justify-between items-center border-t-2 pt-2">
+                        <a href="#" class="text-gray-800 text-sm font-semibold hover:text-purple-600 mr-4">Log in</a>
+                        <a href="#"
+                           class="text-gray-800 text-sm font-semibold border px-4 py-1 rounded-lg hover:text-purple-600 hover:border-purple-600">Register</a>
                     </div>
                 </div>
             </div>
         </div>
+    </div>
 </template>
 
 <script>
