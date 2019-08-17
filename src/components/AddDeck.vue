7a2831b774889a2ba8ab32293ee8896ab19be2da
alexander <al@alvox.net>
2019-08-17 16:57:37 -0400

    Styling for the AddDeck view.

--- src/components/AddDeck.vue
+++ src/components/AddDeck.vue
@@ -1,19 +1,22 @@
 <template>
-    <div class="mx-auto max-w-sm border rounded py-4 px-4 my-10">
-        <p class="text-5xl text-gray-700 text-center">Add Deck</p>
-        <form @submit.prevent="AddDeck">
-            <div class="mb-4">
-                <label class="block text-gray-700 text-sm font-bold mb-2" for="name">Name</label>
-                <input class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="name" type="text" v-model="name">
-            </div>
-            <div class="mb-6">
-                <label class="block text-gray-700 text-sm font-bold mb-2" for="description">Description</label>
-                <input class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="description" type="text" v-model="description">
-            </div>
-            <div class="object-none object-center">
-                <button class="border rounded py-2 px-2 w-1/2">Add Deck</button>
-            </div>
-        </form>
+    <div class="mx-auto max-w-sm bg-white rounded-lg mt-10">
+        <div class="border-2 border-gray-400 rounded-lg rounded-b-none">
+            <p class="text-base font-semibold m-4 pt-1 text-gray-800">Create new collection</p>
+        </div>
+            <form class="border-2 border-gray-400 rounded-lg border-t-0 rounded-t-none" @submit.prevent="AddDeck">
+                <div class="mb-6 p-4 pt-6">
+<!--                    <label class="block text-gray-700 text-sm font-bold mb-2" for="name">Name</label>-->
+                    <input class="appearance-none outline-none border-gray-400 border-b-2 w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="name" type="text" placeholder="Give it a name" v-model="name">
+                </div>
+                <div class="mb-6 p-4">
+<!--                    <label class="block text-gray-700 text-sm font-bold mb-2" for="description">Description</label>-->
+                    <input class="outline-none border-gray-400 border-b-2 w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="description" type="text" placeholder="You can also add some brief description" v-model="description">
+                </div>
+                <div class="flex justify-end p-4 content-center">
+                    <p class="mr-4 cursor-pointer py-2" title="Cancel">Cancel</p>
+                    <button class="border rounded-lg py-2 px-6" title="Create">Create</button>
+                </div>
+            </form>
     </div>
 </template>
 
