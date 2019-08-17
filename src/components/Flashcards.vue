6cc797715109cd6343fa20a33ceae2f0790334a5
alexander <al@alvox.net>
2019-08-17 13:58:15 -0400

    Style flashcards page.

--- src/components/Flashcards.vue
+++ src/components/Flashcards.vue
@@ -1,17 +1,25 @@
 <template>
-    <div class="flashcards mx-auto max-w-5xl">
-        <div class="block flex justify-between">
-            <p class="text-3xl text-gray-700">{{ deck.name }}</p>
-            <a href="" class="border px-4 py-2 rounded-lg">
-                <router-link :to="{name: 'AddFlashcard', params: {deck_id: this.$route.params.deck_id}}">Add new
-                    flashcard
-                </router-link>
-            </a>
-            <span class="text-xl text-red-400" @click="deleteDeck">Delete deck</span>
+    <div class="mx-auto max-w-2xl bg-white rounded-lg mt-10">
+        <div class="flex justify-between border-2 rounded-lg border-b-0 rounded-b-none border-gray-400">
+            <p class="text-base font-semibold m-4 pt-1 text-gray-800">{{ deck.name }}</p>
+            <div class="m-4">
+                <p class="mr-4 cursor-pointer inline-block" title="Delete collection" @click="deleteDeck">
+                    <svg class="fill-current text-red-400 inline-block h-8 w-8" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20">
+                        <path d="M6 2l2-2h4l2 2h4v2H2V2h4zM3 6h14l-1 14H4L3 6zm5 2v10h1V8H8zm3 0v10h1V8h-1z"></path>
+                    </svg>
+                </p>
+                <a href="" title="Create new flashcard">
+                    <router-link :to="{name: 'AddFlashcard', params: {deck_id: this.$route.params.deck_id}}">
+                        <svg class="fill-current text-gray-800 inline-block h-8 w-8" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20">
+                            <path d="M11 9h4v2h-4v4H9v-4H5V9h4V5h2v4zm-1 11a10 10 0 1 1 0-20 10 10 0 0 1 0 20zm0-2a8 8 0 1 0 0-16 8 8 0 0 0 0 16z"></path>
+                        </svg>
+                    </router-link>
+                </a>
+            </div>
         </div>
-        <div class="flex flex-wrap items-stretch">
-            <div class="w-1/3 p-2 self-stretch" v-for="flashcard in deck.cards" :key="flashcard.id">
-                <div class="relative flex-1 text-gray-700 bg-gray-200 rounded px-4 py-2 m-2">
+        <div class="flex flex-wrap items-stretch border-2 rounded-lg rounded-t-none border-gray-400 pt-4 pl-4">
+            <div class="w-1/2 self-stretch" v-for="flashcard in deck.cards" :key="flashcard.id">
+                <div class="relative flex-1 text-gray-700 bg-gray-100 border-2 border-gray-400 rounded-lg rounded px-4 py-2 mb-4 mr-4">
                     <p class="text-2xl">{{ flashcard.front }}</p>
                     <p> {{ flashcard.rear }}</p>
                     <div class="absolute top-0 right-0 px-2 py-2 font-semibold text-xs">
@@ -56,7 +64,5 @@
 </script>
 
 <style>
-    .flashcards {
-        margin-top: 60px;
-    }
+
 </style>
--- src/components/Index.vue
+++ src/components/Index.vue
@@ -1,14 +1,16 @@
 <template>
-    <div class="index mx-auto max-w-2xl bg-white rounded-lg">
+    <div class="mx-auto max-w-2xl bg-white rounded-lg mt-10">
         <div class="flex justify-between border-2 rounded-lg border-b-0 rounded-b-none border-gray-400">
             <p class="text-base font-semibold m-4 pt-1 text-gray-800">My Collections</p>
-            <a href="" class="m-4">
+            <a href="" class="m-4" title="Create new collection">
                 <router-link :to="{name: 'AddDeck'}">
-                    <svg class="fill-current text-gray-800 inline-block h-8 w-8" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"><path d="M11 9h4v2h-4v4H9v-4H5V9h4V5h2v4zm-1 11a10 10 0 1 1 0-20 10 10 0 0 1 0 20zm0-2a8 8 0 1 0 0-16 8 8 0 0 0 0 16z"/></svg>
+                    <svg class="fill-current text-gray-800 inline-block h-8 w-8" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20">
+                        <path d="M11 9h4v2h-4v4H9v-4H5V9h4V5h2v4zm-1 11a10 10 0 1 1 0-20 10 10 0 0 1 0 20zm0-2a8 8 0 1 0 0-16 8 8 0 0 0 0 16z"></path>
+                    </svg>
                 </router-link>
             </a>
         </div>
-        <div class="items-stretch border-2 rounded-lg rounded-t-none border-gray-400 min-h-full shadow">
+        <div class="items-stretch border-2 rounded-lg rounded-t-none border-gray-400 min-h-full">
             <div class="block self-stretch" v-for="deck in decks" :key="deck.id">
                 <router-link :to="{name: 'Flashcards', params: {deck_id: deck.id, deck_name: deck.name}}">
                     <div class="flex-1 bg-gray-100 border-2 border-gray-400 rounded-lg px-4 py-2 m-4 cursor-pointer hover:bg-gray-300 h-40 min-h-full relative">
@@ -42,7 +44,5 @@
 </script>
 
 <style>
-    .index {
-        margin-top: 40px;
-    }
+
 </style>
