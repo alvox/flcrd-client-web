531c7a7e809e12a4aa5327366fe370c4a689480e
alvox <al@alvox.net>
2019-09-01 19:32:43 -0400

    Styles for the 'AddFlashcard' form.

--- src/components/AddDeck.vue
+++ src/components/AddDeck.vue
@@ -3,26 +3,34 @@
         <div class="border-2 border-gray-400 rounded-lg rounded-b-none">
             <p class="text-base font-semibold m-4 pt-1 text-gray-800">Create new collection</p>
         </div>
-            <form class="border-2 border-gray-400 rounded-lg border-t-0 rounded-t-none block" autocomplete="off" @submit.prevent="AddDeck">
-                <div class="p-4 pt-6">
-                    <label class="block text-gray-700 text-sm font-bold mb-1" for="name">Name</label>
-                    <input class="appearance-none outline-none border-2 border-gray-400 rounded-lg w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:border-purple-400" id="name" type="text" placeholder="Give it a name" v-model="name">
-                </div>
-                <div class="p-4 pt-6">
-                    <label class="block text-gray-700 text-sm font-bold mb-1" for="description">Description</label>
-                    <textarea class="appearance-none outline-none focus:outline-none resize-y border-2 rounded-lg py-2 px-3 w-full text-gray-700 leading-tight focus:border-purple-400" id="description" placeholder="You can also add some brief description" rows="4"></textarea>
-                </div>
-                <label class="block text-gray-700 font-bold pl-4">
-                    <input class="mr-2 leading-tight" type="checkbox" v-model="isPublic">
-                    <span class="text-sm">
+        <form class="border-2 border-gray-400 rounded-lg border-t-0 rounded-t-none block" autocomplete="off"
+              @submit.prevent="AddDeck">
+            <div class="p-4 pt-6">
+                <label class="block text-gray-700 text-sm font-bold mb-1" for="name">Name</label>
+                <input class="appearance-none outline-none border-2 border-gray-400 rounded-lg w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:border-purple-400"
+                       id="name" type="text" placeholder="Give it a name" v-model="name">
+            </div>
+            <div class="p-4 pt-6">
+                <label class="block text-gray-700 text-sm font-bold mb-1" for="description">Description</label>
+                <textarea
+                        class="appearance-none outline-none focus:outline-none resize-y border-2 rounded-lg py-2 px-3 w-full text-gray-700 leading-tight focus:border-purple-400"
+                        id="description" placeholder="You can also add some brief description" rows="4"
+                        v-model="description"></textarea>
+            </div>
+            <label class="block text-gray-700 font-bold pl-4">
+                <input class="mr-2 leading-tight" type="checkbox" v-model="isPublic">
+                <span class="text-sm">
                         Make it public
                     </span>
-                </label>
-                <div class="flex justify-end p-4 content-center">
-                    <p class="mr-4 cursor-pointer py-2 text-gray-600 text-sm hover:text-red-500" title="Cancel" @click="GoBack">Cancel</p>
-                    <button class="border rounded-lg py-2 px-6 text-gray-700 text-sm font-bold hover:text-purple-600 hover:border-purple-600" title="Create">Create</button>
-                </div>
-            </form>
+            </label>
+            <div class="flex justify-end p-4 content-center">
+                <p class="mr-4 cursor-pointer py-2 text-gray-600 text-sm hover:text-red-500" title="Cancel"
+                   @click="GoBack">Cancel</p>
+                <button class="border rounded-lg py-2 px-6 text-gray-700 text-sm font-bold hover:text-purple-600 hover:border-purple-600"
+                        title="Create">Create
+                </button>
+            </div>
+        </form>
     </div>
 </template>
 
@@ -51,6 +59,4 @@
     }
 </script>
 
-<style>
-
-</style>
+<style></style>
--- src/components/AddFlashcard.vue
+++ src/components/AddFlashcard.vue
@@ -1,17 +1,28 @@
 <template>
-    <div class="mx-auto max-w-sm border rounded py-4 px-4 my-10">
-        <p class="text-5xl text-gray-700 text-center">Add Flashcard</p>
-        <form @submit.prevent="AddFlashcard">
-            <div class="mb-4">
-                <label class="block text-gray-700 text-sm font-bold mb-2" for="front">Front side</label>
-                <input class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="front" type="text" v-model="front">
+    <div class="mx-auto max-w-sm bg-white rounded-lg mt-10">
+        <div class="border-2 border-gray-400 rounded-lg rounded-b-none">
+            <p class="text-base font-semibold m-4 pt-1 text-gray-800">Add Flashcard</p>
+        </div>
+        <form class="border-2 border-gray-400 rounded-lg border-t-0 rounded-t-none block" autocomplete="off"
+              @submit.prevent="AddFlashcard">
+            <div class="p-4 pt-6">
+                <label class="block text-gray-700 text-sm font-bold mb-1" for="front">Front side</label>
+                <textarea
+                        class="appearance-none outline-none focus:outline-none resize-y border-2 rounded-lg py-2 px-3 w-full text-gray-700 leading-tight focus:border-purple-400"
+                        id="front" placeholder="Front..." rows="4" v-model="front"></textarea>
             </div>
-            <div class="mb-6">
-                <label class="block text-gray-700 text-sm font-bold mb-2" for="rear">Rear side</label>
-                <input class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="rear" type="text" v-model="rear">
+            <div class="p-4 pt-6">
+                <label class="block text-gray-700 text-sm font-bold mb-1" for="rear">Rear side</label>
+                <textarea
+                        class="appearance-none outline-none focus:outline-none resize-y border-2 rounded-lg py-2 px-3 w-full text-gray-700 leading-tight focus:border-purple-400"
+                        id="rear" placeholder="Rear..." rows="4" v-model="rear"></textarea>
             </div>
-            <div class="object-none object-center">
-                <button class="border rounded py-2 px-2 w-1/2">Save</button>
+            <div class="flex justify-end p-4 content-center">
+                <p class="mr-4 cursor-pointer py-2 text-gray-600 text-sm hover:text-red-500" title="Cancel"
+                   @click="GoBack">Cancel</p>
+                <button class="border rounded-lg py-2 px-6 text-gray-700 text-sm font-bold hover:text-purple-600 hover:border-purple-600"
+                        title="Create">Save
+                </button>
             </div>
         </form>
     </div>
@@ -33,11 +44,12 @@
                     front: this.front,
                     rear: this.rear
                 })
+            },
+            GoBack() {
+                this.$router.back()
             }
         }
     }
 </script>
 
-<style scoped>
-
-</style>
+<style></style>
