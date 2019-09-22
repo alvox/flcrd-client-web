9eaed535991d66eade4c69b3faf899356c3084a6
alvox <al@alvox.net>
2019-09-22 19:53:07 -0400

    Theme switching transition.

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,5 @@
 <template>
-    <div id="app" class="min-h-screen flex flex-col bg-background-secondary" :class="theme">
+    <div id="app" class="min-h-screen flex flex-col bg-background-secondary content-wrapper" :class="theme">
         <Navbar/>
         <div class="flex-grow">
             <router-view/>
