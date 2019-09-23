2b166e0c7d17833c200068ea22b9e56b17033ee7
alvox <al@alvox.net>
2019-09-22 20:32:20 -0400

    Icon for theme-switch.

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -44,15 +44,14 @@
                         <a href="" class="primary-btn-outline hover:text-white hover:bg-gray-700">Register</a>
                     </router-link>
                 </div>
-                <div class="sm:hidden mt-4 text-sm text-gray-800">
-                    <p>Theme: Light</p>
-                    <p>Language: EN</p>
-                </div>
-                <button class="text-gray-700 hidden md:block md:ml-2" @click="switchTheme">
-                    <svg class="fill-current h-6 w-6" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20">
-                        <path d="M10 12a2 2 0 1 1 0-4 2 2 0 0 1 0 4zm0-6a2 2 0 1 1 0-4 2 2 0 0 1 0 4zm0 12a2 2 0 1 1 0-4 2 2 0 0 1 0 4z"/>
+                <a href="" class="text-gray-700 block mt-4 md:mt-0 md:ml-4" @click.prevent="switchTheme">
+                    <svg v-if="theme === 'theme-light'" class="fill-current h-5 w-5" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="24" height="24">
+                        <path class="heroicon-ui" d="M9.57 3.38a8 8 0 0 0 10.4 10.4 1 1 0 0 1 1.31 1.3 10 10 0 1 1-13-13 1 1 0 0 1 1.3 1.3zM7.1 5.04A8 8 0 1 0 18.3 16.27 10 10 0 0 1 7.08 5.04z"/>
                     </svg>
-                </button>
+                    <svg v-else class="fill-current h-5 w-5" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20">
+                        <path d="M10 14a4 4 0 1 1 0-8 4 4 0 0 1 0 8zM9 1a1 1 0 1 1 2 0v2a1 1 0 1 1-2 0V1zm6.65 1.94a1 1 0 1 1 1.41 1.41l-1.4 1.4a1 1 0 1 1-1.41-1.41l1.4-1.4zM18.99 9a1 1 0 1 1 0 2h-1.98a1 1 0 1 1 0-2h1.98zm-1.93 6.65a1 1 0 1 1-1.41 1.41l-1.4-1.4a1 1 0 1 1 1.41-1.41l1.4 1.4zM11 18.99a1 1 0 1 1-2 0v-1.98a1 1 0 1 1 2 0v1.98zm-6.65-1.93a1 1 0 1 1-1.41-1.41l1.4-1.4a1 1 0 1 1 1.41 1.41l-1.4 1.4zM1.01 11a1 1 0 1 1 0-2h1.98a1 1 0 1 1 0 2H1.01zm1.93-6.65a1 1 0 1 1 1.41-1.41l1.4 1.4a1 1 0 1 1-1.41 1.41l-1.4-1.4z"/>
+                    </svg>
+                </a>
             </div>
         </nav>
     </div>
@@ -72,6 +71,9 @@
             },
             userName() {
                 return this.$store.getters.userName
+            },
+            theme() {
+                return this.$store.getters.theme
             }
         },
         methods: {
