540ab37aeb2dc69495c7238a8d253e4224eaf84d
alvox <al@alvox.net>
2019-09-29 10:15:48 -0400

    Fix footer alignment finally.

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,11 +1,11 @@
 <template>
     <div class="mx-auto max-w-2xl text-center">
-        <div class="flex items-center mt-10 mb-5 text-copy-secondary text-xs font-thin tracking-wide">
+        <div class="md:flex items-center mt-10 mb-5 text-copy-secondary text-xs font-thin tracking-wide">
+            <p class="text-sm">🚧</p>
             <p>
-                <span class="text-sm">🚧</span>WIP by
-                <a class="underline" href="https://github.com/alvox" target="_blank" rel="noopener">@alvox</a>
+                WIP by <a class="underline" href="https://github.com/alvox" target="_blank" rel="noopener">@alvox</a>
             </p>
-            <p class="px-2 text-base">•</p>
+            <p class="md:px-2 text-base">•</p>
             <router-link :to="{name: 'Terms'}">
                 <p class="underline">Terms and Privacy</p>
             </router-link>
@@ -16,7 +16,7 @@
                          viewBox="0 0 20 20">
                         <path d="M18 2a2 2 0 0 1 2 2v12a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2V4c0-1.1.9-2 2-2h16zm-4.37 9.1L20 16v-2l-5.12-3.9L20 6V4l-10 8L0 4v2l5.12 4.1L0 14v2l6.37-4.9L10 14l3.63-2.9z"/>
                     </svg>
-                    <span class="pl-1 underline">Contact me</span>
+                    <span class="inline-block pl-1 underline">Contact me</span>
                 </a>
             </p>
         </div>
