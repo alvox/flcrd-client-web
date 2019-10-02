a21f1aa5a015399052597f7d4025a1bfe2a17aba
alvox <al@alvox.net>
2019-10-02 07:40:36 -0400

    Use github docker registry.

--- env/build.sh
+++ env/build.sh
@@ -5,6 +5,6 @@ cd ..
 npm run build
 cd env
 cp -r ../dist ./
-docker build -t docker.pkg.github.com/alvox/flcrd-client-web/flcrd-client-web:latest .
-docker push docker.pkg.github.com/alvox/flcrd-client-web/flcrd-client-web:latest
+docker build -t docker.pkg.github.com/alvox/flcrd-client-web/flcrd-webapp:latest .
+docker push docker.pkg.github.com/alvox/flcrd-client-web/flcrd-webapp:latest
 rm -rf dist
