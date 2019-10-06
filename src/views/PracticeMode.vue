17f74fda1ce418c8669a7cb57b5ce7f56342ee0f
alvox <al@alvox.net>
2019-10-06 16:39:47 -0400

    Fix dark mode for the practice results screen.

--- src/views/PracticeMode.vue
+++ src/views/PracticeMode.vue
@@ -60,19 +60,19 @@
                 </div>
             </div>
 <!-- RESULTS -->
-            <div v-else class="block border-2 border-border-primary rounded-b-lg bg-white text-center">
-                <p class="mt-20 text-4xl font-bold tracking-wide text-gray-800">Done!</p>
+            <div v-else class="block border-2 border-border-primary rounded-b-lg bg-background-secondary text-copy-secondary text-center">
+                <p class="mt-20 text-4xl font-bold tracking-wide">Done!</p>
                 <p v-if="allCorrect" class="text-4xl">👏 🎉 🤓</p>
-                <p v-if="allCorrect" class="mt-5 text-gray-800">You've remembered all cards from this
+                <p v-if="allCorrect" class="mt-5">You've remembered all cards from this
                     deck!</p>
-                <p v-else class="text-center mt-5 text-gray-800">You've remembered {{correctAnswers.length}}
+                <p v-else class="text-center mt-5">You've remembered {{correctAnswers.length}}
                     {{correctAnswers.length === 1 ? 'card' : 'cards'}} out of {{deck.cards.length}}.</p>
                 <div class="flex mx-4 md:mx-32 my-10 mb-20">
-                    <p class="flex-1 border border-gray-700 p-2 mr-10 text-center font-extrabold text-gray-700 rounded cursor-pointer hover:text-white hover:bg-gray-700"
+                    <p class="flex-1 border border-gray-700 p-2 mr-10 text-center font-extrabold text-copy-secondary rounded cursor-pointer hover:text-white hover:bg-gray-700"
                        :class="{'ml-10' : correctAnswers.length === 0 || wrongAnswers.length === 0}"
                        @click="repeat(false)">Repeat all cards</p>
                     <p v-if="correctAnswers.length !== 0 && wrongAnswers.length !== 0"
-                       class="flex-1 border border-gray-700 p-2 ml-10 text-center font-extrabold text-gray-700 rounded cursor-pointer hover:text-white hover:bg-gray-700"
+                       class="flex-1 border border-gray-700 p-2 ml-10 text-center font-extrabold text-copy-secondary rounded cursor-pointer hover:text-white hover:bg-gray-700"
                        @click="repeat(true)">
                         Repeat {{wrongAnswers.length}} {{wrongAnswers.length === 1 ? 'card' : 'cards'}}</p>
                 </div>
