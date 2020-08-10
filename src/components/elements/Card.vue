33a8d16316f5b444e1e160a283e93dacfeb905ad
alvox <al@alvox.net>
2020-08-10 16:28:24 -0400

    Use object-contain to fit image in the card's container.

--- src/components/elements/Card.vue
+++ src/components/elements/Card.vue
@@ -2,20 +2,20 @@
     <div class="md:flex text-copy-primary mb-4 mr-4 rounded-lg bg-background-ternary hover:bg-background-hover h-56"
          :class="{'cursor-pointer': belongs_to_user}">
 
-        <div class="flex-1">
-            <div v-if="flashcard.front.startsWith('http')">
-                <img class="object-fill rounded-b-lg md:rounded-r-lg md:rounded-l-none h-56 w-full" :src="flashcard.front" alt="front"/>
+        <div class="flex-1 border-2 border-border-primary rounded-t-lg md:rounded-l-lg md:rounded-r-none">
+            <div v-if="flashcard.front.startsWith('http')" class="h-full">
+                <img class="object-contain h-full w-full" :src="flashcard.front" alt="front"/>
             </div>
-            <div v-else class="flex items-center justify-center p-4 border-2 border-border-primary rounded-t-lg md:rounded-l-lg md:rounded-r-none h-full">
+            <div v-else class="flex items-center justify-center p-4 h-full">
                 <p class="text-base text-center whitespace-pre-line">{{ flashcard.front }}</p>
             </div>
         </div>
 
-        <div class="flex-1">
-            <div v-if="flashcard.rear.startsWith('http')">
-                <img class="object-fill rounded-b-lg md:rounded-r-lg md:rounded-l-none h-56 w-full" :src="flashcard.rear" alt="rear"/>
+        <div class="flex-1 border-2 border-border-primary rounded-b-lg md:rounded-r-lg md:rounded-l-none border-t-0 md:border-t-2 md:border-l-0">
+            <div v-if="flashcard.rear.startsWith('http')" class="h-full">
+                <img class="object-contain h-full w-full" :src="flashcard.rear" alt="rear"/>
             </div>
-            <div v-else class="flex items-center justify-center p-4 border-2 border-border-primary rounded-b-lg md:rounded-r-lg md:rounded-l-none border-t-0 md:border-t-2 md:border-l-0 h-full">
+            <div v-else class="flex items-center justify-center p-4 h-full">
                 <p class="text-base text-center whitespace-pre-line">{{ flashcard.rear }}</p>
             </div>
         </div>
