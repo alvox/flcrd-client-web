a16ddef2410eaddb0e2f5b8f45237b36b1d66697
alvox <al@alvox.net>
2020-08-11 06:24:45 -0400

    Fix mobile layout for the flashcards list view.

--- src/components/elements/Card.vue
+++ src/components/elements/Card.vue
@@ -1,21 +1,21 @@
 <template>
-    <div class="md:flex text-copy-primary mb-4 mr-4 rounded-lg bg-background-ternary hover:bg-background-hover h-56"
+    <div class="md:flex md:h-56 text-copy-primary mb-4 mr-4 rounded-lg bg-background-ternary hover:bg-background-hover"
          :class="{'cursor-pointer': belongs_to_user}">
 
-        <div class="flex-1 border-2 border-border-primary rounded-t-lg md:rounded-l-lg md:rounded-r-none">
-            <div v-if="flashcard.front.startsWith('http')" class="h-full">
+        <div class="flex-1">
+            <div v-if="flashcard.front.startsWith('http')" class="h-full border-2 border-border-primary">
                 <img class="object-contain h-full w-full" :src="flashcard.front" alt="front"/>
             </div>
-            <div v-else class="flex items-center justify-center p-4 h-full">
+            <div v-else class="flex items-center justify-center p-4 h-full border-2 border-border-primary rounded-t-lg md:rounded-l-lg md:rounded-r-none">
                 <p class="text-base text-center whitespace-pre-line">{{ flashcard.front }}</p>
             </div>
         </div>
 
-        <div class="flex-1 border-2 border-border-primary rounded-b-lg md:rounded-r-lg md:rounded-l-none border-t-0 md:border-t-2 md:border-l-0">
-            <div v-if="flashcard.rear.startsWith('http')" class="h-full">
+        <div class="flex-1">
+            <div v-if="flashcard.rear.startsWith('http')" class="h-full border-2 border-border-primary border-t-0 md:border-t-2 md:border-l-0">
                 <img class="object-contain h-full w-full" :src="flashcard.rear" alt="rear"/>
             </div>
-            <div v-else class="flex items-center justify-center p-4 h-full">
+            <div v-else class="flex items-center justify-center p-4 h-full border-2 border-border-primary border-t-0 md:border-t-2 md:border-l-0 rounded-b-lg md:rounded-r-lg md:rounded-l-none">
                 <p class="text-base text-center whitespace-pre-line">{{ flashcard.rear }}</p>
             </div>
         </div>
