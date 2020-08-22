7709b6f85515ed0cce2ee6e3d0728c0bee2cd1d3
alvox <al@alvox.net>
2020-08-22 10:23:47 -0400

    Move dockerfile and build script to the root of the repo.

--- build.sh
+++ build.sh
@@ -1,10 +1,6 @@
 #!/bin/bash
 
 rm -rf dist
-cd ..
 npm run build
-cd env
-cp -r ../dist ./
 docker build -t docker.pkg.github.com/alvox/flcrd-client-web/flcrd-webapp:latest .
 docker push docker.pkg.github.com/alvox/flcrd-client-web/flcrd-webapp:latest
-rm -rf dist
